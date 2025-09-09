22f5743eb6022a9e4a17374ccd8c75a4810bf42c
kempel <ebbkem-2@student.ltu.se>
2025-09-09 15:07:52 +0200

    FIXED dataset

--- d0038e.py
+++ d0038e.py
@@ -1,14 +1,12 @@
-from ucimlrepo import fetch_ucirepo 
-  
-# fetch dataset 
-algerian_forest_fires = fetch_ucirepo(id=547) 
-  
-# data (as pandas dataframes) 
-X = algerian_forest_fires.data.features 
-y = algerian_forest_fires.data.targets 
-  
-# metadata 
-print(algerian_forest_fires.metadata) 
-  
-# variable information 
-print(algerian_forest_fires.variables) 
+import csv
+
+numbers = []
+
+with open("Algerian_forest_fires_dataset_FIXED.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+         numbers.append([int(x) for x in row])
+
+         
+
+
